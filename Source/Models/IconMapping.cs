1562a25f40df25126f199d0bbce241ee102953b3
robloo <robert.looyengoed@gmail.com>
2021-09-13 16:36:02 -0400

    Complete IconMapping

--- Source/Models/IconMapping.cs
+++ Source/Models/IconMapping.cs
@@ -1,4 +1,7 @@
-﻿namespace IconManager
+﻿using IconManager.Models.Serialization;
+using Newtonsoft.Json;
+
+namespace IconManager
 {
     /// <summary>
     /// Defines a mapping from the source icon to the destination icon.
@@ -7,22 +10,96 @@
     /// </summary>
     public class IconMapping
     {
+        /***************************************************************************************
+         *
+         * Constructors
+         *
+         ***************************************************************************************/
+
+        public IconMapping()
+        {
+            this.Source               = new Icon();
+            this.Destination          = new Icon();
+            this.GlyphMatchQuality    = MatchQuality.NoMatch;
+            this.MetaphorMatchQuality = MatchQuality.NoMatch;
+            this.IsPlaceholder        = false;
+        }
+
         public IconMapping(IIcon source, IIcon destination)
         {
-            this.Source      = source;
-            this.Destination = destination;
+            this.Source               = source;
+            this.Destination          = destination;
+            this.GlyphMatchQuality    = MatchQuality.NoMatch;
+            this.MetaphorMatchQuality = MatchQuality.NoMatch;
+            this.IsPlaceholder        = false;
         }
+
+        /***************************************************************************************
+         *
+         * Property Accessors
+         *
+         ***************************************************************************************/
+
+        ///////////////////////////////////////////////////////////
+        // Data
+        ///////////////////////////////////////////////////////////
 
         /// <summary>
         /// Gets or sets the source icon to map from.
         /// This must contain a Unicode point in hexadecimal format with no prefix.
         /// </summary>
+        [JsonProperty(Order = 2)]
         public IIcon Source { get; set; }
 
         /// <summary>
         /// Gets or sets the destination icon to map to.
         /// This must contain a Unicode point in hexadecimal format with no prefix.
         /// </summary>
+        [JsonProperty(Order = 1)]
         public IIcon Destination { get; set; }
+
+        /// <summary>
+        /// Gets or sets the quality of match for the glyphs of the two icons.
+        /// </summary>
+        [JsonProperty(Order = 3)]
+        [JsonConverter(typeof(MatchQualityConverter))]
+        public MatchQuality GlyphMatchQuality { get; set; }
+
+        /// <summary>
+        /// Gets or sets the quality of match for the metaphor (purpose) of the two icons.
+        /// </summary>
+        [JsonProperty(Order = 4)]
+        [JsonConverter(typeof(MatchQualityConverter))]
+        public MatchQuality MetaphorMatchQuality { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the mapping had no direct equivalent
+        /// so a generic substitution was made.
+        /// </summary>
+        [JsonProperty(Order = 5)]
+        public bool IsPlaceholder { get; set; }
+
+        /// <summary>
+        /// Gets or sets comments describing the mapping and any decisions or special considerations.
+        /// </summary>
+        [JsonProperty(Order = 6)]
+        public string Comments { get; set; }
+
+        ///////////////////////////////////////////////////////////
+        // Calculated
+        ///////////////////////////////////////////////////////////
+
+        /// <summary>
+        /// Gets a value indicating weather the mapping is considered valid
+        /// (both a source and destination icon are defined).
+        /// </summary>
+        [JsonIgnore]
+        public bool IsValid
+        {
+            get => this.Source != null &&
+                   string.IsNullOrEmpty(this.Source.UnicodePoint) == false &&
+                   this.Destination != null &&
+                   string.IsNullOrEmpty(this.Destination.UnicodePoint) == false;
+        }
     }
 }
---
+++ Source/Models/MatchQuality.cs
@@ -0,0 +1,54 @@
+﻿namespace IconManager
+{
+    /// <summary>
+    /// Defines the quality of a metaphor or glyph match between two icons.
+    /// </summary>
+    public enum MatchQuality
+    {
+        /// <summary>
+        /// There is no match between two icons.
+        /// </summary>
+        /// <remarks>
+        /// Generally 'NoMatch' should not be used and instead no association (mapping)
+        /// between two such icons should be made.
+        /// </remarks>
+        NoMatch = 0,
+
+        /// <summary>
+        /// Two icons are loosely similar in metaphor or glyph and there are significant
+        /// differences. The user may have difficulty recognizing the similarity.
+        /// </summary>
+        /// <remarks>
+        /// Significant differences are common if a substitution was made.
+        /// These may be good candidates to retrieve from another icon set instead.
+        /// </remarks>
+        Low,
+
+        /// <summary>
+        /// Two icons are moderately similar in metaphor or glyph.
+        /// The user can usually recognize the similarity.
+        /// </summary>
+        Medium,
+
+        /// <summary>
+        /// Two icons have a very high degree of similarity in metaphor or glyph.
+        /// The user can perfectly recognize the similarity.
+        /// </summary>
+        /// <remarks>
+        /// Example 1: One icon has rounded edges and the other square, but all
+        ///            other characteristics in the glyph are the same.
+        /// Example 2: One glyph is slightly larger or smaller than the other.
+        /// </remarks>
+        High,
+
+        /// <summary>
+        /// Two icons are exactly the same in metaphor or glyph.
+        /// The user can perfectly recognize the similarity.
+        /// </summary>
+        /// <remarks>
+        /// Exact means there are absolutely no discernible differences between two icons.
+        /// This value is very rarely used.
+        /// </remarks>
+        Exact
+    }
+}
