a123329278221bab73ce500581bbf258579ae90a
robloo <robert.looyengoed@gmail.com>
2021-09-16 12:03:10 -0400

    Separate GlyphSource

--- Source/Models/FontBuilder.cs
+++ Source/Models/FontBuilder.cs
@@ -28,41 +28,6 @@ namespace IconManager
         private static object directoryMutex = new object();
         private static object cacheMutex     = new object();
 
-        /// <summary>
-        /// Defines a potential source for a glyph.
-        /// </summary>
-        private enum GlyphSource
-        {
-            /// <summary>
-            /// No glyph source is available.
-            /// </summary>
-            None = 0,
-
-            /// <summary>
-            /// The glyph is available in an existing font file.
-            /// </summary>
-            /// <remarks>
-            /// This is suitable for use when building a font.
-            /// </remarks>
-            FontFile,
-
-            /// <summary>
-            /// The glyph is available as an image in an online (downloadable) file.
-            /// </summary>
-            /// <remarks>
-            /// This is suitable to preview the glyph only, it cannot be used to build a font.
-            /// </remarks>
-            OnlineImage,
-
-            /// <summary>
-            /// The glyph is available in an SVG file.
-            /// </summary>
-            /// <remarks>
-            /// This is suitable for use when building a font.
-            /// </remarks>
-            SvgFile
-        }
-
         /***************************************************************************************
          *
          * Constructors
---
+++ Source/Models/GlyphSource.cs
@@ -0,0 +1,37 @@
+﻿namespace IconManager
+{
+    /// <summary>
+    /// Defines a potential source for a glyph.
+    /// </summary>
+    public enum GlyphSource
+    {
+        /// <summary>
+        /// No glyph source is available.
+        /// </summary>
+        None = 0,
+
+        /// <summary>
+        /// The glyph is available in an existing font file.
+        /// </summary>
+        /// <remarks>
+        /// This is suitable for use when building a font.
+        /// </remarks>
+        FontFile,
+
+        /// <summary>
+        /// The glyph is available as an image in an online (downloadable) file.
+        /// </summary>
+        /// <remarks>
+        /// This is suitable to preview the glyph only, it cannot be used to build a font.
+        /// </remarks>
+        OnlineImage,
+
+        /// <summary>
+        /// The glyph is available in an SVG file.
+        /// </summary>
+        /// <remarks>
+        /// This is suitable for use when building a font.
+        /// </remarks>
+        SvgFile
+    }
+}
