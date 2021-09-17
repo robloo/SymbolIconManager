f70df205425672fbb8bae9f3d15f65686c4f87d4
robloo <robert.looyengoed@gmail.com>
2021-09-17 11:24:30 -0400

    Remove remarks moved to docs

--- Source/Models/MatchQuality.cs
+++ Source/Models/MatchQuality.cs
@@ -6,22 +6,14 @@
     public enum MatchQuality
     {
         /// <summary>
-        /// There is no match between two icons.
+        /// There is no discernible match between two icons.
         /// </summary>
-        /// <remarks>
-        /// Generally 'NoMatch' should not be used and instead no association (mapping)
-        /// between two such icons should be made.
-        /// </remarks>
         NoMatch = 0,
 
         /// <summary>
         /// Two icons are loosely similar in metaphor or glyph and there are significant
         /// differences. The user may have difficulty recognizing the similarity.
         /// </summary>
-        /// <remarks>
-        /// Significant differences are common if a substitution was made.
-        /// These may be good candidates to retrieve from another icon set instead.
-        /// </remarks>
         Low,
 
         /// <summary>
@@ -34,21 +26,12 @@
         /// Two icons have a very high degree of similarity in metaphor or glyph.
         /// The user can perfectly recognize the similarity.
         /// </summary>
-        /// <remarks>
-        /// Example 1: One icon has rounded edges and the other square, but all
-        ///            other characteristics in the glyph are the same.
-        /// Example 2: One glyph is slightly larger or smaller than the other.
-        /// </remarks>
         High,
 
         /// <summary>
         /// Two icons are exactly the same in metaphor or glyph.
         /// The user can perfectly recognize the similarity.
         /// </summary>
-        /// <remarks>
-        /// Exact means there are absolutely no discernible differences between two icons.
-        /// This value is very rarely used.
-        /// </remarks>
         Exact
     }
 }
