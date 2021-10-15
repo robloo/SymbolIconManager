264bdb932dfdd8c3a73b63536f2e24251c8bddb6
robloo <robert.looyengoed@gmail.com>
2021-10-15 15:58:42 -0400

    Add IconSetFamily enum

--- Source/Models/IconSets/IconSet.cs
+++ Source/Models/IconSets/IconSet.cs
@@ -18,62 +18,75 @@
 
         /// <summary>
         /// The cross-platform Fluent UI System icons with filled theme.
-        /// Family: FluentUISystem.
-        /// https://github.com/microsoft/fluentui-system-icons
         /// </summary>
+        /// <remarks>
+        /// Family: <see cref="IconSetFamily.FluentUISystem"/>.
+        /// </remarks>
         FluentUISystemFilled,
 
         /// <summary>
         /// The cross-platform Fluent UI System icons with regular theme.
-        /// Family: FluentUISystem.
-        /// https://github.com/microsoft/fluentui-system-icons
         /// </summary>
+        /// <remarks>
+        /// Family: <see cref="IconSetFamily.FluentUISystem"/>.
+        /// </remarks>
         FluentUISystemRegular,
 
         /// <summary>
         /// The cross-platform, open-source Line Awesome icons with company brand style.
-        /// Family: LineAwesome.
-        /// https://github.com/icons8/line-awesome
         /// </summary>
+        /// <remarks>
+        /// Family: <see cref="IconSetFamily.LineAwesome"/>.
+        /// </remarks>
         LineAwesomeBrand,
 
         /// <summary>
         /// The cross-platform, open-source Line Awesome icons with regular style.
-        /// Family: LineAwesome.
-        /// https://github.com/icons8/line-awesome
         /// </summary>
+        /// <remarks>
+        /// Family: <see cref="IconSetFamily.LineAwesome"/>.
+        /// </remarks>
         LineAwesomeRegular,
 
         /// <summary>
         /// The cross-platform, open-source Line Awesome icons with solid fill style.
-        /// Family: LineAwesome.
-        /// https://github.com/icons8/line-awesome
         /// </summary>
+        /// <remarks>
+        /// Family: <see cref="IconSetFamily.LineAwesome"/>.
+        /// </remarks>
         LineAwesomeSolid,
 
         /// <summary>
         /// The Windows 10/11 Segoe Fluent icons (Windows symbols version 3).
-        /// https://docs.microsoft.com/en-us/windows/apps/design/signature-experiences/iconography#system-icons
         /// </summary>
+        /// <remarks>
+        /// Family: <see cref="IconSetFamily.SegoeFluent"/>.
+        /// </remarks>
         SegoeFluent,
 
         /// <summary>
         /// The Windows 10 Segoe MDL2 Assets icons (Windows symbols version 2).
-        /// https://docs.microsoft.com/en-us/windows/apps/design/style/segoe-ui-symbol-font
         /// </summary>
+        /// <remarks>
+        /// Family: <see cref="IconSetFamily.SegoeMDL2Assets"/>.
+        /// </remarks>
         SegoeMDL2Assets,
 
         /// <summary>
         /// The Windows 8 Segoe UI Symbol icons (Windows symbols version 1).
         /// </summary>
+        /// <remarks>
+        /// Family: <see cref="IconSetFamily.SegoeUISymbol"/>.
+        /// </remarks>
         SegoeUISymbol,
 
         /// <summary>
         /// An open source icon font fallback to Microsoft's <see cref="SegoeUISymbol"/> and
         /// <see cref="SegoeMDL2Assets"/> that was provided by the Microsoft WinJS team.
-        /// https://github.com/microsoft/fonts
-        /// https://github.com/winjs/winjs/tree/master/src/fonts
         /// </summary>
+        /// <remarks>
+        /// Family: <see cref="IconSetFamily.WinJSSymbols"/>.
+        /// </remarks>
         WinJSSymbols,
     }
 }
---
+++ Source/Models/IconSets/IconSetFamily.cs
@@ -0,0 +1,63 @@
+﻿namespace IconManager
+{
+    /// <summary>
+    /// Defines a named family of icon sets.
+    /// </summary>
+    public enum IconSetFamily
+    {
+        /// <inheritdoc cref="IconSet.Undefined"/>
+        Undefined = 0,
+
+        /// <summary>
+        /// The cross-platform Fluent UI System icons.
+        /// </summary>
+        /// <remarks>
+        /// https://github.com/microsoft/fluentui-system-icons.
+        /// Includes both <see cref="IconSet.FluentUISystemRegular"/> and
+        /// <see cref="IconSet.FluentUISystemFilled"/>.
+        /// </remarks>
+        FluentUISystem,
+
+        /// <summary>
+        /// The cross-platform, open-source Line Awesome icons.
+        /// </summary>
+        /// <remarks>
+        /// https://github.com/icons8/line-awesome.
+        /// </remarks>
+        LineAwesome,
+
+        /// <summary>
+        /// The Windows 10/11 Segoe Fluent icons (Windows symbols version 3).
+        /// </summary>
+        /// <remarks>
+        /// https://docs.microsoft.com/en-us/windows/apps/design/style/segoe-fluent-icons-font.
+        /// </remarks>
+        SegoeFluent,
+
+        /// <summary>
+        /// The Windows 10 Segoe MDL2 Assets icons (Windows symbols version 2).
+        /// </summary>
+        /// <remarks>
+        /// https://docs.microsoft.com/en-us/windows/apps/design/style/segoe-ui-symbol-font.
+        /// </remarks>
+        SegoeMDL2Assets,
+
+        /// <summary>
+        /// The Windows 8 Segoe UI Symbol icons (Windows symbols version 1).
+        /// </summary>
+        /// <remarks>
+        /// https://docs.microsoft.com/en-us/previous-versions/windows/apps/jj841127(v=win.10).
+        /// </remarks>
+        SegoeUISymbol,
+
+        /// <summary>
+        /// An open source icon font fallback to Microsoft's <see cref="SegoeUISymbol"/> and
+        /// <see cref="SegoeMDL2Assets"/> that was provided by the Microsoft WinJS team.
+        /// </summary>
+        /// <remarks>
+        /// https://github.com/microsoft/fonts;
+        /// https://github.com/winjs/winjs/tree/master/src/fonts.
+        /// </remarks>
+        WinJSSymbols,
+    }
+}
